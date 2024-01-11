69f09731a2dfb46f7662d0f5a91a4c464c68becb
Cuteva1 <guragaineva27@gmail.com>
2024-01-11 14:32:15 +0545

    Authentication and Premission- api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import hello_world,student_info,StudentGetAPIView,StudentListAPIView,StudentListView, \
     StudentCreateView,StudentUpdateView,StudentDeleteView, StudentListCreateView, \
@@ -23,6 +24,7 @@ generic_urls =[
     path("generic-student-delete/<int:pk>/", StudentDeleteView.as_view()),
     path("generic-student-list-create/", StudentListCreateView.as_view()),
     path("generic-student-retrive-update-delete/<int:pk>/", StudentRetriveUpdateDeleteView.as_view()),
+    path("login/", obtain_auth_token)
 ]
 
 urlpatterns +=generic_urls + router.urls
